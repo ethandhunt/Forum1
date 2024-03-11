0bf784b851ade7888f3993cad0ffd0a8d7a02004
ethandhunt <46333321+ethandhunt@users.noreply.github.com>
2024-03-12 12:48:28 +1300

    move forum.php css from style.css to forum.css

--- forum.php
+++ forum.php
@@ -134,6 +134,7 @@ if (!isset($_SESSION["user_id"])) {
 <head>
     <title> Forum </title>
     <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/forum.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="includes/vote.js"></script>
 </head>
