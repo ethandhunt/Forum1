28ce1c491df8bf18427da29727d77c1b21d533ef
ethandhunt <46333321+ethandhunt@users.noreply.github.com>
2024-03-28 12:14:21 +1300

    update last_address field

--- includes/db.php
+++ includes/db.php
@@ -33,7 +33,8 @@ if (isset($_SESSION["user_id"])) {
         $conn->query("UPDATE users SET register_address='$ip_addr' WHERE user_id=$user_id");
     }
 
-    // update last_online field
-    $conn->query("UPDATE users SET last_online=NOW() WHERE user_id=$user_id");
+    // update last_online and last_address field
+    $addr = $_SERVER["REMOTE_ADDR"];
+    $conn->query("UPDATE users SET last_online=NOW(), last_address='$addr' WHERE user_id=$user_id");
 }
 ?>
