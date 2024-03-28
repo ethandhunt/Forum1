ba02c022537a72b017785a811ebe3b9b3a8710c5
zamiel.clayton <claytonzamiel10@gmail.com>
2024-03-29 11:01:29 +1300

    modified:   includes/header.php 	modified:   style/style.css

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,56 @@
 <div class="header">
     <img src="images/logo.jpg" class="logo">
-    <div>
+    <div class="title">
         Forum 1
     </div>
 
-    <ul class="nav-links">
-         <li><a href="forum.php">Home</a></li>
-         <li><a>JFOKEL</a></li>
-         <li><a></a></li>
-         <li><a>ehj</a></li>
-     </ul>   
+    <div class="navbar-container">
+        <ul>
+        <li>
+            <a href="index.php"> Home </a>
+            <?php
+            if (!isset($_SESSION["user_id"])) {
+                ?>
+                <a href="register.php"> Register </a>
+                <a href="login.php"> Login </a>
+                <?php
+            }
+            if (isset($_SESSION["user_id"])) {
+                ?>
+                <a href="logout.php"> Logout </a>
+                <a href="forum.php"> Forum </a>
+                <a href="account.php"> Account </a>
+                <?php
+            }
+            if (isset($_SESSION["banned"]) && !$_SESSION["banned"]) {
+                ?>
+                <a href="post.php"> Post </a>
+                <a href="chat.php?view=chatrooms"> Live Chat </a>
+                <?php
+            }
+            ?>
+            <a href="bug_report.php"> Bug report </a>
+            <a href="changelog.php"> Changelog </a>
+            <?php
+            if (isset($_SESSION['administrator']) && $_SESSION['administrator']) {
+                ?>
+                <a href="admin.php"> Admin </a>
+                <?php
+            }
+            if (isset($_SESSION['moderator']) && $_SESSION['moderator']) {
+                ?>
+                <a href="reports.php"> Reports </a>
+                <?php
+            }
+            if (isset($_SESSION["banned"]) && $_SESSION["banned"]) {
+                ?>
+                <a class="ban-message" href="banned.php">
+                    You are banned
+                </a>
+                <?php
+            }
+            ?>
+        </li>
+        </ul>   
+    </div>
 </div>
