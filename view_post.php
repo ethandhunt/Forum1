7e850e067a0dd29f9d0d896f9c2f2ca63c5207e3
zamiel.clayton <claytonzamiel10@gmail.com>
2024-04-04 22:56:33 +1300

    changed post image (forgot to change before)

--- view_post.php
+++ view_post.php
@@ -351,8 +351,8 @@ if ($author["administrator"]) {
                     <?php
                 }
                 ?>
-                <img class="post-image" src="<?php echo filter_var(htmlentities($post["image_href"], ENT_QUOTES), FILTER_SANITIZE_URL) ?>">
             </div>
+            <img class="post-image" src="<?php echo filter_var(htmlentities($post["image_href"], ENT_QUOTES), FILTER_SANITIZE_URL) ?>">
             <?php
             if ($author_id == $_SESSION["user_id"]) {
                 ?>
